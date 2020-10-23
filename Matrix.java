48fccaf815583a3f78b4834d33f081e9430c0061
kirillnsb <73110914+kirillnsb@users.noreply.github.com>
2020-10-23 22:29:48 +0300

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -1,12 +1,9 @@
-package matrixworker;
+package kirillnsb.matrixworker;
 
 import java.util.Arrays;
 
-public class Matrix implements IMatrixWorker {
+public class Matrix {
     private double[][] matrix;
-
-    public Matrix() {
-    }
 
     public Matrix(double[][] m) {
         this.matrix = m;
@@ -16,65 +13,8 @@ public class Matrix implements IMatrixWorker {
         return matrix;
     }
 
-    @Override
-    public void print(double[][] m) {
-        for (int i = 0; i < m.length; i++) {
-            for (int j = 0; j < m[i].length; j++) {
-                System.out.print(m[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-    public void print() {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    @Override
-    public boolean haveSameDimension(double[][] m1, double[][] m2) {
-        return m1.length == m2.length && m1[0].length == m2[0].length;
-    }
-
-    @Override
-    public double[][] add(double[][] m1, double[][] m2) {
-        double[][] c = new double[m1.length][m1[0].length];
-        for (int i = 0; i < m1.length; i++) {
-            for (int j = 0; j < m1[i].length; j++) {
-                c[i][j] = m1[i][j] + m2[i][j];
-            }
-        }
-        return c;
-    }
-
-    @Override
-    public double[][] subtract(double[][] m1, double[][] m2) { //m1 - m2
-        double[][] c = new double[m1.length][m1[0].length];
-        for (int i = 0; i < m1.length; i++) {
-            for (int j = 0; j < m1[i].length; j++) {
-                c[i][j] = m1[i][j] - m2[i][j];
-            }
-        }
-        return c;
-    }
-
-    @Override
-    public double[][] multiply(double[][] m1, double[][] m2) { // m1 * m2
-        double[][] c = null;
-        if (m1[0].length == m2.length) {                 //conformity check
-            c = new double[m1.length][m2[0].length];
-            for (int i = 0; i < m1.length; ++i) {
-                for (int k = 0; k < m2[0].length; ++k) {
-                    for (int j = 0; j < m1[0].length; j++) {
-                        c[i][k] += m1[i][j] * m2[j][k];
-                    }
-                }
-            }
-        }
-        return c;
+    public void setMatrix(double[][] matrix) {
+        this.matrix = matrix;
     }
 
     @Override
@@ -89,9 +29,10 @@ public class Matrix implements IMatrixWorker {
     public int hashCode() {
         return Arrays.hashCode(matrix);
     }
+
     @Override
-    public String toString() { //custom toString
-        String str="";
+    public String toString() {
+        String str = "";
         for (int i = 0; i < matrix.length; i++) {
             str += "[";
             for (int j = 0; j < matrix[i].length; j++) {
@@ -101,37 +42,4 @@ public class Matrix implements IMatrixWorker {
         }
         return str;
     }
-
-/* default IDE toString version
-    @Override
-    public String toString() {
-        return "Matrix{" +
-                "matrix=" + Arrays.toString(matrix) +
-                '}';
-    }
-*/
-    /*
-    public static void main(String[] args) {
-
-        double[][] m1 = {{1, 2, 3},
-                         {4, 3, 2}};
-
-        double[][] m2 = {{3, 5},
-                         {1, 2},
-                         {3, 4}};
-
-        Matrix mat1 = new Matrix(m1);                                          //
-        Matrix mat2 = new Matrix(m1);                                          //
-        Matrix mat3 = new Matrix(m2);                                          //
-        System.out.println("mat1(m1) == mat2(m1) " + mat1.equals(mat2));       //
-        System.out.println("mat1(m1) == mat3(m2) " + mat1.equals(mat3));       //equals check
-
-        System.out.println("mat1.hashCode " + mat1.hashCode());        //
-        System.out.println("  m1 hashCode " + Arrays.hashCode(m1));    //hashCode check
-
-        System.out.println(mat1.toString());
-
-    }
-    */
 }
-
